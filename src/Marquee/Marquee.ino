2bb32551bf418916b25b64f5955cd948e0737a60
asil <asil.yihong@gmail.com>
2014-03-22 23:17:55 +0800

    Use busy loop, and add switch text.
    1. Detect switch button press status, and change text.
    2. Use busy loop for detect switch button status.

--- src/Marquee/Marquee.ino
+++ src/Marquee/Marquee.ino
@@ -3,10 +3,13 @@
 
 #include "fonts.h"
 
-#define DELAY_INTERVAL 160
-char DisplayWord[] = "HELLO!!";
+#define DELAY_COUNT 150
+#define FLASH_INTERVAL 400
+#define FLASH_COUNT 2
+char *DisplayWords[] = {"HELLO!!", "Openlab.Taipei", "Honki!!"};
+const int INSTANCE_CNT = 3;
 const byte SS_SIZE = 2;
-const byte SS_SET[] = {10, 9, 8, 7, 6, 5, 4, 3, 2};
+const byte SS_SET[] = {10, 9, 8, 7, 6, 5, 4, 3};
 
 const byte NOOP = 0x0;
 const byte DECODEMODE = 0x9;
@@ -15,8 +18,37 @@ const byte SCANLIMIT = 0xB;
 const byte SHUTDOWN = 0xC;
 const byte DISPLAYTEST = 0xF;
 
+int switchFlag = 0;
+int instanceIdx = 0;
+int delayCnt = 0;
 byte index = 0;
 byte TOTAL_LEN;
+char *DisplayWord;
+
+void switchText(int idx) {
+  byte k, j, i;
+  for (i = 0; i < FLASH_COUNT; i++) {
+    for (k = 0; k < SS_SIZE; k++) {
+      for (j = 0; j < 8; j++) {
+        max7219(SS_SET[k], j + 1, 0xFF);
+      }
+    }
+    delay(FLASH_INTERVAL);
+    for (k = 0; k < SS_SIZE; k++) {
+      for (j = 0; j < 8; j++) {
+        max7219(SS_SET[k], j + 1, 0x00);
+      }
+    }
+    delay(FLASH_INTERVAL);
+  }
+  DisplayWord = DisplayWords[idx];
+  char *str = DisplayWord;
+  while(*str) {
+    str++;
+  }
+  TOTAL_LEN = ((int)(str - DisplayWord)) << 3;
+  index = delayCnt = 0;
+}
 
 void max7219(byte pin, byte reg, byte data) {
   digitalWrite(pin, LOW);
@@ -27,12 +59,8 @@ void max7219(byte pin, byte reg, byte data) {
 
 void setup() {
   byte k, i;
-  char *str = DisplayWord;
-  while(*str) {
-    str++;    
-  }
-  TOTAL_LEN = ((int)(str - DisplayWord)) << 3;
 
+  pinMode(2, INPUT);
   for (k = 0; k < SS_SIZE; k++) {
     pinMode(SS_SET[k], OUTPUT);
     digitalWrite(SS_SET[k], HIGH);
@@ -50,11 +78,21 @@ void setup() {
       max7219(SS_SET[k], i + 1, 0);
     }
   }
+  
+  switchText(0);
 }
 
 void loop() {
   byte j, k, offset;
   int idx = 0;
+  int switchInput = digitalRead(2);
+  if (switchInput == 1 && switchInput != switchFlag) {
+    switchFlag = switchInput;
+    instanceIdx = (instanceIdx + 1) % INSTANCE_CNT;
+    switchText(instanceIdx);
+  } else if (switchInput != switchFlag) {
+    switchFlag = switchInput;
+  }
   for (k = 0; k < SS_SIZE; k++) {
     offset = k << 3;
     for (j = 0; j < 8; j++) {
@@ -62,7 +100,9 @@ void loop() {
       max7219(SS_SET[k], j + 1, fonts[(int)(DisplayWord[idx >> 3] - 32)][idx & 7]);
     }
   }
-  delay(DELAY_INTERVAL);
-  index = (index + 1) % TOTAL_LEN;
+  if (delayCnt++ >= DELAY_COUNT) {
+    delayCnt = 0;
+    index = (index + 1) % TOTAL_LEN;
+  }
 }
 
